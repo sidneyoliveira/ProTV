6ae428f682ca32a9437b7fe55382786dadeffe28
sidneyoliveira <sidneyoliveira2015@gmail.com>
2023-11-06 22:34:56 -0300

    Adicionado o Slide para a transição das imagens

--- pro_TV/src/App.jsx
+++ pro_TV/src/App.jsx
@@ -3,16 +3,48 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import { Swiper, SwiperSlide } from 'swiper/react'
+
+import { EffectFade, Navigation, Pagination } from 'swiper/modules';
+
 function App() {
-  const [count, setCount] = useState(0)
+  const data = [
+  {id: '1', image: '/src/assets/a1.jpg'},
+  {id: '2', image: '/src/assets/a2.jpg'},
+  {id: '3', image: '/src/assets/a3.jpg'},
+  {id: '4', image: '/src/assets/a4.jpg'}
+  ]
 
   return (
     <>
-    <div className="App">
+    <div className="container">
+      <Swiper
+        loop={true}
+        autoplay={{
+            delay: 3000,
+            disableOnInteraction: false
+        }}
+        effect={'fade'}
+        navigation={false}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[EffectFade, Navigation, Pagination]}
+      >
+        {data.map((item) => (
+          <SwiperSlide key={item.id}>
+            <img 
+              src={item.image}
+              alt="img" 
+              className="slide-img"
+            />
 
-        <h1>
+          </SwiperSlide>
           
-        </h1>
+        ))}
+        
+
+      </Swiper>
 
     </div>
     </>
--- pro_TV/src/main.jsx
+++ pro_TV/src/main.jsx
@@ -2,6 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/effect-fade';
+import 'swiper/css/pagination';
+import 'swiper/css/autoplay';
+
+
+import { register } from 'swiper/element/bundle'
+
+register();
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
